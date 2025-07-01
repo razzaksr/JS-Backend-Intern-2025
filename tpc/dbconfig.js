94cce6a6c094d470dd9e6171df9b0df222762f42
razzaksr <razzaksr@gmail.com>
2025-07-01 10:22:47 +0530

    CRUD done with usecase also env implemented

--- tpc/dbconfig.js
+++ tpc/dbconfig.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
 
 const connectDb = async() => {
     try{
-        await mongoose.connect("mongodb+srv://razak:mohamed@cluster0.ptmlylq.mongodb.net/internship?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("database is connected!!!!!!")
     }
     catch(err){
--- tpc/requirement.js
+++ tpc/requirement.js
@@ -16,4 +16,41 @@ router.get('/view',async(req,res)=>{
     res.json(await Campus.find())
 })
 
+// delete campus via _id
+router.delete('/cancel/:id',async(req,res)=>{
+    await Campus.findOneAndDelete({_id:req.params.id})
+    res.json({"message":`${req.params.id} has been cancelled`})
+})
+
+// add participants by _id
+router.patch('/register',async(req,res)=>{
+    const{primary,candidate} = req.body
+    const tempCamp = await Campus.findOne({_id:primary})
+    if(tempCamp&&tempCamp.recruited.length>0)
+        res.status(400).json({error:`${primary} already held`})        
+    const one = await Campus.findOneAndUpdate(
+        {_id:primary},
+        {$addToSet:{participants:candidate}},
+        {new:true}
+    )
+    if(!one)
+        res.status(404).json({error:`${primary} not available`})
+    res.json({message:`${primary} updated`})
+})
+
+// declare who are all selected
+router.patch('/results',async(req,res)=>{
+    const{primary,candidates} = req.body
+    const tempCamp = await Campus.findOne({_id:primary})
+    if(tempCamp&&tempCamp.recruited.length>0)
+        res.status(400).json({error:`${primary} already held`})
+    candidates.map((each,index)=>{
+        if(tempCamp.participants.includes(each))
+            tempCamp.recruited.push(each)
+    })
+    // update since _id already available
+    await tempCamp.save()
+    res.json({message:`${tempCamp.company} has dcone with their recruitement`})
+})
+
 module.exports = router
